37bee5d5834e1fc89915090fe8bac00ad3382a78
Andi Kolari <AndiKolari627@gmail.com>
2024-09-02 20:16:17 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Calendar from "./components/Calendar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import Event from "./Event";
+
+const Calendar = () => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="time">8 am</td>
+            <Event event="Fancy Dinner 🎩" color="green" />
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">9 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event="Gaming " color="pink" />
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">10 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">11 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">12 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">1 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">2 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">3 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event="Doing Codepath" color="pink" />
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">4 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event="Fancier Dinner " color="blue" />
+          </tr>
+
+          <tr>
+            <td className="time">5 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event="Super Dinner" color="pink" />
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Calendar;
